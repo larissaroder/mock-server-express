8a6e055dfa97a0afd68d1413ae64fa7f601395b7
Larissa Bonifacio Roder <lalarissaroder@Larissas-MacBook-Pro.local>
2020-01-17 21:34:49 -0300

    Change por environment

--- src/server.js
+++ src/server.js
@@ -4,9 +4,9 @@ import '../env/env';
 
 const app = express();
 
-const server = () => app.use(router).listen(global.gConfig.node_port, () => {
+const server = () => app.use(router).listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`${global.gConfig.config_id} listening on port ${global.gConfig.node_port}`);
+  console.log(`${global.gConfig.config_id} listening on port ${process.env.PORT}`);
 });
 
 server();
